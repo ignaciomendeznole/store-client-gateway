92c4833c351f81641ca9d73093079b3d678d80de
Ignacio Mendez Nole <ignaciomendeznole@gmail.com>
2024-03-24 23:43:39 -0300

    Refactor AuthService to use async/await and handle exceptions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,43 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { RegisterUserDto, LoginUserDto } from './dto';
+import { firstValueFrom } from 'rxjs';
+import { RpcCustomExceptionFilter } from 'src/common/exceptions/rpc-custom-exception.filter';
 
 @Injectable()
 export class AuthService {
   constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
 
   async registerUser(registerUserDto: RegisterUserDto) {
-    return this.natsClient.send('auth.register.user', registerUserDto);
+    try {
+      const res = await firstValueFrom(
+        this.natsClient.send('auth.register.user', registerUserDto),
+      );
+      return res;
+    } catch (error) {
+      throw new RpcCustomExceptionFilter();
+    }
   }
 
   async loginUser(loginUserDto: LoginUserDto) {
-    return this.natsClient.send('auth.login.user', loginUserDto);
+    try {
+      const res = await firstValueFrom(
+        this.natsClient.send('auth.login.user', loginUserDto),
+      );
+      return res;
+    } catch (error) {
+      throw new RpcCustomExceptionFilter();
+    }
   }
 
   async verifyToken(token: string) {
-    return this.natsClient.send('auth.verify.user', token);
+    try {
+      const res = await firstValueFrom(
+        this.natsClient.send('auth.verify.token', { token }),
+      );
+      return res;
+    } catch (error) {
+      throw new RpcCustomExceptionFilter();
+    }
   }
 }
