4dd8965c19746d45d7ea08f6ef6157248c7677a4
Ignacio Mendez Nole <ignaciomendeznole@gmail.com>
2024-03-25 00:51:14 -0300

    Add authentication verification endpoint and related decorators and guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +17,14 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.loginUser(loginUserDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return {
+      user,
+      token,
+    };
   }
 }
